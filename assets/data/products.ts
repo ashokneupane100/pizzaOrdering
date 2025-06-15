147abe70c6459a406eca4d0974a3098b6b4c4109
Ashok Neupane <ashokabrother@gmail.com>
2025-06-16 04:49:05 +0545

    changed in to Nepali Rupees #15.17

--- assets/data/products.ts
+++ assets/data/products.ts
@@ -4,70 +4,70 @@ const products = [
     name: 'Ultimate Pepperoni',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: 12.99,
+    price: (12.99 * 133.5).toFixed(0), // 1734 NPR
   },
   {
     id: 2,
     name: 'ExtravaganZZa',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: 14.99,
+    price: (14.99 * 133.5).toFixed(0), // 2000 NPR
   },
   {
     id: 3,
     name: 'MeatZZa',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: 13.47,
+    price: (13.47 * 133.5).toFixed(0), // 1798 NPR
   },
   {
     id: 4,
     name: 'Margarita',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
-    price: 9.9,
+    price: (9.9 * 133.5).toFixed(0), // 1322 NPR
   },
   {
     id: 5,
     name: 'Pacific Veggie',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.99,
+    price: (12.99 * 133.5).toFixed(0), // 1734 NPR
   },
   {
     id: 6,
     name: 'Hawaiian',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
-    price: 10.49,
+    price: (10.49 * 133.5).toFixed(0), // 1400 NPR
   },
   {
     id: 7,
     name: 'Deluxe',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
-    price: 16.99,
+    price: (16.99 * 133.5).toFixed(0), // 2268 NPR
   },
   {
     id: 8,
     name: 'BBQ Chicken',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.89,
+    price: (12.89 * 133.5).toFixed(0), // 1720 NPR
   },
   {
     id: 9,
     name: 'Chicken Bacon Ranch',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: 13.99,
+    price: (13.99 * 133.5).toFixed(0), // 1868 NPR
   },
   {
     id: 10,
     name: '6 Cheese',
     image:
       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
-    price: 13.29,
+    price: (13.29 * 133.5).toFixed(0), // 1774 NPR
   },
 ];
 
