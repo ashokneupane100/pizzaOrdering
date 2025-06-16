39ca84ea8517fb69b9aae6000ae26fdf75e50c08
Ashok Neupane <ashokabrother@gmail.com>
2025-06-16 05:47:28 +0545

    building first styling #27.45

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -51,7 +51,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: "Tab Two",
+          title: "Pizza",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
