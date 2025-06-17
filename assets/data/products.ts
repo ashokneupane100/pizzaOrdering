cc9b0c52119beb0bed45157505a4c51f51a34b2a
Ashok Neupane <ashokabrother@gmail.com>
2025-06-17 07:47:04 +0545

    introduced typeScript and path aliases

--- assets/data/products.ts
+++ assets/data/products.ts
@@ -1,73 +1,73 @@
 const products = [
   {
     id: 1,
-    name: 'Ultimate Pepperoni',
+    name: "Ultimate Pepperoni",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: (12.99 * 133.5).toFixed(0), // 1734 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png",
+    price: Math.round(12.99 * 133.5), // 1734 NPR
   },
   {
     id: 2,
-    name: 'ExtravaganZZa',
+    name: "ExtravaganZZa",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: (14.99 * 133.5).toFixed(0), // 2000 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png",
+    price: Math.round(14.99 * 133.5), // 2000 NPR
   },
   {
     id: 3,
-    name: 'MeatZZa',
+    name: "MeatZZa",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: (13.47 * 133.5).toFixed(0), // 1798 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png",
+    price: Math.round(13.47 * 133.5), // 1798 NPR
   },
   {
     id: 4,
-    name: 'Margarita',
+    name: "Margarita",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
-    price: (9.9 * 133.5).toFixed(0), // 1322 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png",
+    price: Math.round(9.9 * 133.5), // 1322 NPR
   },
   {
     id: 5,
-    name: 'Pacific Veggie',
+    name: "Pacific Veggie",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: (12.99 * 133.5).toFixed(0), // 1734 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png",
+    price: Math.round(12.99 * 133.5), // 1734 NPR
   },
   {
     id: 6,
-    name: 'Hawaiian',
+    name: "Hawaiian",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
-    price: (10.49 * 133.5).toFixed(0), // 1400 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png",
+    price: Math.round(10.49 * 133.5), // 1400 NPR
   },
   {
     id: 7,
-    name: 'Deluxe',
+    name: "Deluxe",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
-    price: (16.99 * 133.5).toFixed(0), // 2268 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png",
+    price: Math.round(16.99 * 133.5), // 2268 NPR
   },
   {
     id: 8,
-    name: 'BBQ Chicken',
+    name: "BBQ Chicken",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: (12.89 * 133.5).toFixed(0), // 1720 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png",
+    price: Math.round(12.89 * 133.5), // 1720 NPR
   },
   {
     id: 9,
-    name: 'Chicken Bacon Ranch',
+    name: "Chicken Bacon Ranch",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: (13.99 * 133.5).toFixed(0), // 1868 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png",
+    price: Math.round(13.99 * 133.5), // 1868 NPR
   },
   {
     id: 10,
-    name: '6 Cheese',
+    name: "6 Cheese",
     image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
-    price: (13.29 * 133.5).toFixed(0), // 1774 NPR
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png",
+    price: Math.round(13.29 * 133.5), // 1774 NPR
   },
 ];
 
